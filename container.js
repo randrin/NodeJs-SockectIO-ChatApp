c84b6557c0b95c0fac02c8af8a5aaba5603ce0ac
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2021-04-21 22:36:25 +0200

    Set up passport

--- container.js
+++ container.js
@@ -3,21 +3,19 @@ const path = require("path");
 
 const container = dependable.container();
 
-const simpleDependencies = [
-    ["_", "lodash"]
-];
+const simpleDependencies = [["_", "lodash"]];
 
-simpleDependencies.forEach(function(val) {
-    container.register(val[0], function() {
-        return require(val[1])
-    })
+simpleDependencies.forEach(function (val) {
+  container.register(val[0], function () {
+    return require(val[1]);
+  });
 });
 
 container.load(path.join(__dirname, "/controllers"));
 container.load(path.join(__dirname, "/helpers"));
 
-container.register("container", function() {
-    return container;
+container.register("container", function () {
+  return container;
 });
 
 module.exports = container;
--- server.js
+++ server.js
@@ -8,62 +8,66 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
-const passport = require("passport")
+const passport = require("passport");
 const dotenv = require("dotenv");
 const container = require("./container");
 const config = require("./config");
 dotenv.config();
 
-
 container.resolve(function (users) {
+  mongoose.Promise = global.Promise;
+  mongoose
+    .connect(config.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log("connected to mongodb");
+    })
+    .catch((error) => {
+      console.log("Error connected to mongodb:", error.reason);
+    });
 
-    mongoose.Promise = global.Promise;
-    mongoose.connect(config.MONGODB_URL, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-        useCreateIndex: true })
-        .then(() => {
-            console.log("connected to mongodb");
-        })
-        .catch((error) => {
-            console.log("Error connected to mongodb:", error.reason);
-        });
+  const app = setUpExpress();
 
-    const app = setUpExpress();
+  function setUpExpress() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(config.PORT, function () {
+      console.log(`Server started at http://localhost:${config.PORT}`);
+    });
+    ConfigureExpress(app);
 
-    function setUpExpress() {
-        const app = express();
-        const server = http.createServer(app);
-        server.listen(config.PORT, function () {
-            console.log(`Server started at http://localhost:${config.PORT}`);
-        });
-        ConfigureExpress(app);
+    //Setup router
+    const router = require("express-promise-router")();
+    users.SetRouting(router);
 
-        //Setup router
-        const router = require("express-promise-router")();
-        users.SetRouting(router);
+    app.use(router);
+  }
 
-        app.use(router);
-    }
+  function ConfigureExpress(app) {
+    require("./passport/passport-local");
 
-    function ConfigureExpress(app) {
-        app.use(express.static("public"));
-        app.use(cookieParser());
-        app.set("view engine", "ejs");
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static("public"));
+    app.use(cookieParser());
+    app.set("view engine", "ejs");
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-        app.use(validator());
-        app.use(session({
-            secret: config.JWT_SECRET,
-            resave: true,
-            saveUninitialized: true,
-            store: new MongoStore({ mongooseConnection: mongoose.connection })
-        }));
+    app.use(validator());
+    app.use(
+      session({
+        secret: config.JWT_SECRET,
+        resave: true,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      })
+    );
 
-        app.use(flash());
+    app.use(flash());
 
-        app.use(passport.initialize());
-        app.use(passport.session());
-    }
+    app.use(passport.initialize());
+    app.use(passport.session());
+  }
 });
